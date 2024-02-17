70a4bc817349029e429a89c4ff63abfdd5493f74
Marcelo Studart <studartmarc@gmail.com>
2024-02-17 15:18:49 -0300

    desenvolvimento final do game detona ralph a partir das aulas do instrutor

---
+++ src/scripts/engine.js
@@ -0,0 +1,67 @@
+alert("Welcome to Ralph mtf game! That's all, fools!")
+
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score")
+    },
+    values: {
+        gameSpeed: 1000,
+        hitPosition: 0,
+        points: 0,
+        currentTime: 60,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+};
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId)
+        clearInterval(state.actions.timerId)
+        alert(`Game Over! O tempo acabou e seu resultado foi ${state.values.points}`)
+    };
+};
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/sounds/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+};
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.points++;
+                state.view.score.textContent = state.values.points;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        });
+    });
+};
+
+function init() {
+    addListenerHitBox();
+};
+
+init();
